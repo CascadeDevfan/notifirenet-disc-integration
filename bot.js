fbbea4579a1297270ac18d707795ef362e768bbf
CascadeDevfan <55059288+CascadeDevfan@users.noreply.github.com>
2019-12-08 14:08:22 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+client.on("ready", () =>{
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setPresence({
+        status: "online",  //You can show online, idle....
+        game: {
+            name: "Thousands of NFN Networks and counting",  //The message shown
+            type: "WATCHING:" //PLAYING: WATCHING: LISTENING: STREAMING:
+        }
+    });
+ });
 
 client.on('message', message => {
     if (message.content === 'ping') {
